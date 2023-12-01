bb5e8a5ce4d99d2258614f9e8c9aaf8937bfba10
Richard Barker <richbarker@gmail.com>
2023-12-02 10:30:11 +1100

    Remove dead code

--- src/aoc_23/day01.clj
+++ src/aoc_23/day01.clj
@@ -9,17 +9,6 @@
 
 (def alpha-nums {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5 "six" 6 "seven" 7 "eight" 8 "nine" 9})
 
-(defn decipher-alphanum-calibration [s]
-  (let [digits     (re-seq #"\d|one|two|three|four|five|six|seven|eight|nine" s)
-        first-digi (first digits)
-        first-digi (get alpha-nums first-digi first-digi)
-
-        last-digi  (last digits)
-        last-digi  (get alpha-nums last-digi last-digi)]
-
-
-    (Integer/parseInt (str first-digi last-digi))))
-
 (defn sum-calibrations [input-strs]
   (reduce + (map decipher-calibration input-strs)))
 
@@ -33,18 +22,6 @@
   (is (= 77 (decipher-calibration "treb7uchet")))
 
   (is (= 142 (sum-calibrations (str/split-lines "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet")))))
-
-(deftest part-2-checks
-  (is (= 11 (decipher-alphanum-calibration "qzjggk1one")))
-  (is (= 14 (decipher-alphanum-calibration "onechldlktgq5xckxbtlhhszjmsjlsttxrncssffh4sccgxjz")))
-  (is (= 33 (decipher-alphanum-calibration "threehpbsevenffnqgdjcftjkdjhhk7dvzmkmqthreefflb")))
-  (is (= 68 (decipher-alphanum-calibration "six11eightoneightklv")))
-
-  (let [test-strs    (str/split-lines "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen")
-        test-checks  [29, 83, 13, 24, 42, 14, 76]
-        calibrations (map decipher-alphanum-calibration test-strs)]
-    (is (= test-checks calibrations))
-    (is (= 281 (reduce + calibrations)))))
 
 (defn starts-with-num?
   "if the string starts with an alpha num, return the numeric number"
