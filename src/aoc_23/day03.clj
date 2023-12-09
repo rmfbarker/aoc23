8a33fab04abf60d614fd24eb83635341d0fea29f
Richard Barker <richbarker@gmail.com>
2023-12-09 18:01:12 +1100

    Add function to check the row before

--- src/aoc_23/day03.clj
+++ src/aoc_23/day03.clj
@@ -7,16 +7,18 @@
   (re-seq #"\d+" s))
 
 (def sample-file "sample-03.txt")
-(def input-file "input-03.txt")
+(def input-file "input-03")
 
 (defn read-file [f] (str/split-lines (slurp (io/resource f))))
 
 (def sample (read-file sample-file))
 
-(def indexed-lines (map
-                     vector
-                     (range)
-                     sample))
+(defn create-index [s] (map
+                         vector
+                         (range)
+                         s))
+
+(def indexed-lines (create-index sample))
 
 ;; The below does not handle lines with repeated numbers because it uses 'substring'
 (defn parse-row [row-line]
@@ -51,24 +53,44 @@
       (recur (+ 1 idx pointer) (conj acc idx))
       acc)))
 
+(defn check-row-before [indexed-lines row col digit])
+
 
 (deftest part1-tests
   (is (= [0 5] (indexes-of
                  "346..346...*.....475.440....903&..996*...404+.395...*..............*.......&253.223.....................453..535......@....265.....290$........"
                  "346")))
   (is (= [0] (indexes-of
-                 "346.....*.....475.440....903&..996*...404+.395...*..............*.......&253.223.....................453..535......@....265.....290$........"
-                 "346")))
+               "346.....*.....475.440....903&..996*...404+.395...*..............*.......&253.223.....................453..535......@....265.....290$........"
+               "346")))
   (is (= [0 3] (indexes-of
                  "346346...*.....475.440....903&..996*...404+.395...*..............*.......&253.223.....................453..535......@....265.....290$........"
-                 "346"))))
+                 "346")))
 
-  ;; get the series of indexes that i need to check for a symbol.
+  (let [indexed-file (create-index (read-file sample-file))
+        [row line] (nth indexed-file 2)
+        digits       (get-digits line)
+        digit        (first digits)
+        idxs         (indexes-of line digit)]
+    ;; look at the row before this line and check if there is a symbol touching the number
+    (check-row-before indexed-file row (first idxs) digit))
 
 
+  (is (true? (check-row-before (read-file sample-file) 2 2 "35"))))
 
+;; get the series of indexes that i need to check for a symbol.
 
+;; for each line, get the list of numbers, get their indexes, look around those numbers for a symbol, keep numbers that are touching a symbol. sum those numbers that match that filter.
+(defn sum-numbers [file-name]
+  (let [indexed-file (create-index (read-file file-name))]
+    (doseq [line indexed-file]
+      (let [[row data] line
+            digits (get-digits data)]
+        (doseq [digit digits
+                idx   (indexes-of data digit)]
+          (println "row" row "col" idx "digit" digit))))))
 
+(sum-numbers sample-file)
 
 
 ;; test lines with duplicate numbers
